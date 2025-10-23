df60b2e06885d1d6051df340e0f7740198c3f17a
Mareedu Saibabu <53231064+msboffl@users.noreply.github.com>
2025-10-23 10:32:28 +0530

    Add solution - CodeHub

---
+++ 6-kyu/playing-with-digits/playing-with-digits.js
@@ -0,0 +1,11 @@
+function digPow(n, p){
+  const digits = n.toString().split('').map(Number);
+  let sum = 0;
+​
+  for (let i = 0; i < digits.length; i++) {
+    sum += Math.pow(digits[i], p + i);
+  }
+​
+  const k = sum / n;
+  return Number.isInteger(k) ? k : -1;
+}
