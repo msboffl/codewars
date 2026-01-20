66b3734bb6c7b9e5eb0228860e59ab76b31068ff
Mareedu Saibabu <53231064+msboffl@users.noreply.github.com>
2026-01-20 07:11:13 +0530

    Update solution - CodeHub

--- 7-kyu/square-every-digit/square-every-digit.js
+++ 7-kyu/square-every-digit/square-every-digit.js
@@ -1,15 +1,12 @@
 function squareDigits(num){
-  if(num == 0) return 0;
+  let res = "";
   
-  let ans = "";
   while(num > 0) {
     let ld = num % 10;
-    let sq = ld * ld
-    
-    ans = sq + ans;
-    
+    let sq = ld * ld;
+    res = sq + res;
     num = Math.floor(num / 10);
   }
   
-  return Number(ans);
+  return Number(res);
 }
